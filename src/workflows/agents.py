5738de46168afe9740653fc8cbfcd84c95b61cb1
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2025-03-03 09:43:10 +0300

    Feat: Added the grade_documents node and removed structured output because deepseek does not support it

--- src/workflows/agents.py
+++ src/workflows/agents.py
@@ -2,8 +2,8 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
-from prompts import HALLUCINATION_PROMPT, ANSWER_PROMPT, REWRITER_PROMPT, RAG_PROMPT, GRADER_PROMPT
-from models import GradeAnswer, GradeHallucinations, GradeDocuments
+from workflows.prompts import HALLUCINATION_PROMPT, ANSWER_PROMPT, REWRITER_PROMPT, RAG_PROMPT, GRADER_PROMPT
+from workflows.models import GradeAnswer, GradeHallucinations, GradeDocuments
 
 
 load_dotenv(find_dotenv())
@@ -17,18 +17,18 @@ llm = ChatOpenAI(
 
 # create an llm that will check if there are hallucinations
 structured_hallucination_grader = llm.with_structured_output(GradeHallucinations)
-hallucination_grader = HALLUCINATION_PROMPT | structured_hallucination_grader
+hallucination_grader = HALLUCINATION_PROMPT | StrOutputParser() # Didn't try to enfornce structured output because deepseek doesn't support it
 
 # create an llm that will grade if the answer is enough or not
 structured_answer_grader = llm.with_structured_output(GradeAnswer)
-answer_grader = ANSWER_PROMPT | structured_answer_grader
+answer_grader = ANSWER_PROMPT | StrOutputParser() # Didn't try to enfornce structured output because deepseek doesn't support it
 
 # create an llm that will rewrite the question/user-prompt
 question_rewriter = REWRITER_PROMPT | llm | StrOutputParser()
 
 # create an llm that will grade the documents
 structured_document_grader = llm.with_structured_output(GradeDocuments)
-document_grader = GRADER_PROMPT | structured_document_grader
+document_grader = GRADER_PROMPT | llm | StrOutputParser() # Didn't try to enfornce structured output because deepseek doesn't support it
 
 # create an llm that will produce the final answer
 answer_generator = RAG_PROMPT | llm | StrOutputParser()
--- src/workflows/nodes.py
+++ src/workflows/nodes.py
@@ -1,5 +1,6 @@
 from scripts.embedding_service import PineconeEmbeddingManager
 from workflows.states import RAGState
+from langchain_openai import ChatOpenAI
 
 def retrieve_documents(state: RAGState, retriever: PineconeEmbeddingManager) -> RAGState:
     print('--RETRIEVING--')
@@ -8,8 +9,26 @@ def retrieve_documents(state: RAGState, retriever: PineconeEmbeddingManager) -> 
 
     return {"question": question, "documents": documents}
 
-def grade_documents(state: RAGState) -> RAGState:
-    ...
+def grade_documents(state: RAGState, document_grader: ChatOpenAI) -> RAGState:
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state['question']
+    documents = state['documents']
+
+    filtered_docs = []
+    for doc in documents:
+        score = document_grader.invoke({
+            "question": question,
+            "document": doc
+        })
+
+        result = score
+        if result == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(doc)
+        else:
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+    
+    return {"documents": filtered_docs, "question": question}
 
 def generate_response(state: RAGState) -> RAGState:
     ...
